5810189fd1369ae42008edd6e468f890d0238a74
gustavodsilva <gustavo.gsilva83@gmail.com>
2026-04-23 15:20:41 -0300

    Implementa menu e classe Produto para sistema de estoque
    Substitui o código inicial por um menu interativo para cadastro, listagem e baixa de produtos em estoque. Adiciona a classe Produto com propriedades Nome, Quantidade e Preço.

---
+++ EstoqueApp/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace EstoqueApp;
+
+internal class Produto
+{
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public decimal Preco { get; set; }
+}
--- EstoqueApp/Program.cs
+++ EstoqueApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EstoqueApp;
+
+List<Produto> produtos = new List<Produto>();
+
+while (true)
+{
+    Console.WriteLine("\n=== SISTEMA DE ESTOQUE ===");
+    Console.WriteLine("1 - Cadastrar produto");
+    Console.WriteLine("2 - Listar produtos");
+    Console.WriteLine("3 - Dar baixa no estoque");
+    Console.WriteLine("0 - Sair");
+
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            // cadastrar
+            break;
+
+        case "2":
+            // listar
+            break;
+
+        case "3":
+            // dar baixa
+            break;
+
+        case "0":
+            return;
+    }
+}
